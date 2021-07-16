55d2010a3fafde971cee821def6104c70d69e17d
Chris Marston <chris.scott.marston@gmail.com>
2021-07-16 13:59:32 -0600

    Repaired GUI - I deleted it somehow!

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -6,29 +6,24 @@ import javafx.scene.shape.Circle;
 
 public class Controller {
 
-	@FXML // Injects the code from your .fxml file into this class
-	private Circle myCircle;
-	private double x;
-	private double y;
+//	@FXML // Injects the code from your .fxml file into this class
+//	private Circle myCircle;
+//	private double x;
+//	private double y;
 	
-	public void up(ActionEvent e) {
-//		System.out.println("UP");
-		myCircle.setCenterY(y = y - 1);
+	public void budgetButton(ActionEvent e) {
+		System.out.println("Budget");
+//		myCircle.setCenterY(y = y - 1);
 	}
 	
-	public void down(ActionEvent e) {
-//		System.out.println("DOWN");
-		myCircle.setCenterY(y = y + 1);
+	public void statsButton(ActionEvent e) {
+		System.out.println("Stats");
+//		myCircle.setCenterY(y = y + 1);
 	}
 	
-	public void left(ActionEvent e) {
-//		System.out.println("LEFT");
-		myCircle.setCenterX(x = x - 1);
-	}
-	
-	public void right(ActionEvent e) {
-//		System.out.println("RIGHT");
-		myCircle.setCenterX(x = x + 1);
+	public void toBudgetButton(ActionEvent e) {
+		System.out.println("To Budget");
+//		myCircle.setCenterX(x = x - 1);
 	}
 
 }

bd1085bbb623c5664eba0a2f1702b60bfc141567
Chris Marston <chris.scott.marston@gmail.com>
2021-07-20 16:03:05 -0600

    Got basic test UI setup

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -4,11 +4,18 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.shape.Circle;
 
 public class Controller {
-
 	
+	@FXML
+	private Button testButton;
+
+	public void testMethod() {
+		System.out.println("Test successful!");
+		testButton.setText("Test successful!");
+	}
 
 }

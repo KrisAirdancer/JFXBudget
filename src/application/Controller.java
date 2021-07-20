f5f9277da32cf588ff7c16d56d24c710e56481b4
Chris Marston <chris.scott.marston@gmail.com>
2021-07-20 11:14:45 -0600

    Completed part of a JavaFX tutorial

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -9,26 +9,6 @@ import javafx.scene.shape.Circle;
 
 public class Controller {
 
-	@FXML // Injects the code from your .fxml file into this class
-	ListView<String> list;
 	
-	ObservableList<String> items = FXCollections.observableArrayList();
-	
-	
-	
-	public void budgetButton(ActionEvent e) {
-		System.out.println("Budget");
-//		myCircle.setCenterY(y = y - 1);
-	}
-	
-	public void statsButton(ActionEvent e) {
-		System.out.println("Stats");
-//		myCircle.setCenterY(y = y + 1);
-	}
-	
-	public void toBudgetButton(ActionEvent e) {
-		System.out.println("To Budget");
-//		myCircle.setCenterX(x = x - 1);
-	}
 
 }
